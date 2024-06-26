47c7e59f97820b87a4fbf13563f980a30cc17ff9
Zaq Reynolds <zaqreynolds@gmail.com>
2024-06-26 13:39:53 -0700

    setup jest

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,13 @@
+import Home from "@/app/page";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+
+describe("Home", () => {
+  it("renders a heading", () => {
+    render(<Home />);
+
+    const heading = screen.getByRole("heading", { level: 1 });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
