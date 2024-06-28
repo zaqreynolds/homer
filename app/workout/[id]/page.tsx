72a7d867fbcf20610cec1f5e93c25b8dadffba71
Zaq Reynolds <zaqreynolds@gmail.com>
2024-06-28 16:47:16 -0700

    fix(page): Handle valid and invalid workout IDs more effectively
    Improved handling of valid and invalid workout IDs for better user experience. Added error logging and refined error messages for smoother error handling.

--- app/workout/[id]/page.tsx
+++ app/workout/[id]/page.tsx
@@ -1,27 +1,35 @@
 import { dbConnect } from "@/lib/mongo";
 import Workouts from "@/models/workouts";
 import { WorkoutDTO } from "../types";
+import mongoose from "mongoose";
 
 const Workout = async ({ params }: { params: { id: string } }) => {
   const { id } = params;
 
   await dbConnect();
 
-  const workout = await Workouts.findById(id);
-
-  if (!workout) {
+  if (!mongoose.Types.ObjectId.isValid(id)) {
     return <div>No workout found</div>;
   }
+  try {
+    const workout = await Workouts.findById(id);
 
-  return (
-    <div className="grow flex flex-col w-full items-center">
-      <h2 className="text-xl">
-        Day {workout.day}&apos;s Workout: {workout.name}
-        {/* hi */}
-      </h2>
-      {/* Display more details about the workout here */}
-    </div>
-  );
+    if (!workout) {
+      return <div>No workout found</div>;
+    }
+
+    return (
+      <div className="grow flex flex-col w-full items-center">
+        <h2 className="text-xl">
+          Day {workout.day}&apos;s Workout: {workout.name}
+        </h2>
+        {/* Display more details about the workout here */}
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching workout:", error);
+    return <div>An error occurred</div>; // Optionally handle the error more gracefully
+  }
 };
 
 export default Workout;
